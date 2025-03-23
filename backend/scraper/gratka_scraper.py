b18d6dc826a048d1de671251b0e530d6474f0a8b
Patryk Karbowy <patryk.karbowy11@gmail.com>
2025-03-23 14:55:47 +0100

    Added basic frontend + edit on be

--- backend/scraper/gratka_scraper.py
+++ backend/scraper/gratka_scraper.py
@@ -2,9 +2,10 @@ from backend.scraper.base_scraper import BaseScraper
 
 
 class GratkaScraper(BaseScraper):
-    def __init__(self):
+    def __init__(self, city: str):
         super().__init__("https://gratka.pl/nieruchomosci")
+        self.city = city
 
     def scrape(self):
-        self.open()
+        self.open(self.city)
         # self.close()
