3549894d7bcba334562c425dcb0d837f30beab65
Patryk Karbowy <patryk.karbowy11@gmail.com>
2025-05-02 12:08:58 +0200

    Added option to scrap other websites

--- backend/scraper/gratka_scraper.py
+++ backend/scraper/gratka_scraper.py
@@ -3,9 +3,12 @@ from backend.scraper.base_scraper import BaseScraper
 
 class GratkaScraper(BaseScraper):
     def __init__(self, city: str):
-        super().__init__("https://gratka.pl/nieruchomosci")
-        self.city = city
+        super().__init__("https://www.gratka.pl/nieruchomosci/", city)
 
     def scrape(self):
-        self.open(self.city)
-        # self.close()
+        """Scrapes OLX listings for the specified city."""
+        html = self.get_page_source()
+        body_content = self.extract_body_content(html)
+        cleaned_content = self.clean_body_content(body_content)
+
+        return cleaned_content
--- backend/scraper/otodom_scraper.py
+++ backend/scraper/otodom_scraper.py
@@ -3,9 +3,12 @@ from backend.scraper.base_scraper import BaseScraper
 
 class OtodomScraper(BaseScraper):
     def __init__(self, city: str):
-        super().__init__("https://gratka.pl/nieruchomosci")
-        self.city = city
+        super().__init__("https://www.otodom.pl/pl/", city)
 
     def scrape(self):
-        self.open(self.city)
-        # self.close()
+        """Scrapes OLX listings for the specified city."""
+        html = self.get_page_source()
+        body_content = self.extract_body_content(html)
+        cleaned_content = self.clean_body_content(body_content)
+
+        return cleaned_content
